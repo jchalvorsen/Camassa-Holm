1854fc70670fdb69a80755a91b483dfe9944501d
Andreas Longva <andreas@fuwsoft.com>
2014-03-30 10:36:42 +0200

    Expanded animatedplot to support multiple animated lines in the same plot

--- util/animatedplot.m
+++ util/animatedplot.m
@@ -1,18 +1,38 @@
-function [ ] = animatedplot(x, t, U)
+function [ ] = animatedplot(x, t, varargin)
 % Target framerate
 FRAMERATE = 30;
 FRAMELENGTH = 1 / FRAMERATE;
 
+% Every cell in varargin is a matrix of (x, t) data
+% Number of data sets
+N = length(varargin);
+
+% Number of time points
+T = size(varargin{1}, 1);
+for j = 2:N
+    T = max(T, size(varargin{j}, 1));
+end
+
+% Let U hold the data for each time step across all series
+U = zeros(N, length(x));
+for j = 1:N
+    U(j, :) = varargin{j}(1, :);
+end
+
 % Plot initial data
 figure
-plothandle = plot(x, U(1, :));
+plothandle = plot(x, U);
 %Umax = max(U(1, :));
 Umax = max(max(U));
 Umin = min(min(U));
 ylim([Umin, Umax]);
-for j = 2:length(t)
-    set(plothandle, 'YData', U(j, :))
+for j = 2:T
+    for k = 1:N
+        set(plothandle(k), 'YData', varargin{k}(j, :));
+    end
+    
     drawnow
     pause(FRAMELENGTH)
 end
+
 end
