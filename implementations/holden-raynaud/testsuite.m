daabf431741d58b409d5e7b51747720816355b6d
Andreas Longva <andreas@fuwsoft.com>
2014-03-30 10:38:19 +0200

    Moved paper_method to archive

--- implementations/holden-raynaud/testsuite.m
+++ implementations/holden-raynaud/testsuite.m
@@ -9,12 +9,12 @@ clear path;
 
 %% Configuration
 % Spatial resolution
-N = 12000;
+N = 5000;
 % Maximum time value
-T = 20;
+T = 5;
 
-xmin = -20;
-xmax = 20;
+xmin = -10;
+xmax = 10;
 
 % Compression settings
 % nx = number of x values in compressed matrix
@@ -35,12 +35,19 @@ a = 1;
 %initial = @(x) exp(-10 * abs(x - 1.2)) + exp(-10 * abs(x - 0.2));
 %initial = @(x) cosh(min(x, a - x)) / sinh(a / 2);
 %initial = @(x) 3 * exp(-abs(x)) - 3 * exp(-abs(x - 10));
-initial = @(x) exp(-abs(x + 2)) + exp(-abs(x - 2));
+%initial = @(x) exp(-abs(x + 2)) + exp(-abs(x - 2));
 %initial = @(x) 5 * exp(-abs(x + 5)) + 5 * exp(-abs(x - 25));
 %initial = @(x) exp(-abs(x + 5)) - exp(-abs(x - 5));
 %initial = @(x) sin(2 * pi * x / 20) + 1;
 %initial = @(x) sin(pi * x / 2);
-%initial = @(x) exp(-abs(x));
+initial = @(x) exp(-abs(x));
+%initial = @(x) sin(pi * x / 10);
+% m1 = (exp(-5) + 6) / (2 * exp(-5) + 3);
+% m2 = (exp(-5) + 2/3) / (exp(-5) + 3);
+% x1 = log(18 * exp(-10) / (exp(-5)) + 6);
+% x2 = log(40 * exp(-10) + 60 * exp(-5));
+% initial = @(x) m1 * exp(-abs(x - x1)) + ...
+%      m2 * exp(-abs(x - x2));
 
 %% Solve equation
 [U, x, t] = holdenraynaud(N, T, [xmin, xmax], initial);
