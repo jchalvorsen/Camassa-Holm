072a7c8ca03acf8adb974ca1ff920d96513a4d51
Andreas Longva <andreas@fuwsoft.com>
2014-04-01 22:25:16 +0200

    Added semi-discrete implementation (unfinished and ineffective, but works as a prototype)

---
+++ implementations/holden-raynaud/holdenraynaud_semidiscrete.m
@@ -0,0 +1,188 @@
+function [ U, x, t ] = holdenraynaud_semidiscrete(N, T, xdomain, initial, varargin)
+
+[ showprogress, printtiming, timestep ] = parse(varargin);
+
+%% Sanity checks
+assert(isvector(xdomain), 'xdomain must be a vector.');
+assert(length(xdomain) == 2, 'xdomain must have length 2.');
+assert(xdomain(2) > xdomain(1), 'xdomain = [XMIN, XMAX] must have XMAX > XMIN');
+assert(N > 1, 'N must be greater than 1.');
+assert(timestep >= 0, 'Supplied timestep must be non-negative');
+
+%% Configuration
+% Domain of x.
+xmin = xdomain(1);
+xmax = xdomain(2);
+
+%% Preparation
+% Spatial step size
+h = (xmax - xmin) / N;
+
+% x values in grid
+x = xmin + (0:N-1) * h;
+
+% Generate initial data
+initialdata = initial(x);
+
+% Temporal step size
+dt = calculatetimestep(initial, xmin, xmax, h);
+if timestep == 0
+    k = dt;
+else
+    % Use user-supplied timestep
+    k = timestep;
+    if k > dt
+       warning(['User-supplied timestep is larger than CFL-based heuristic. ' ...
+           'Stability may suffer as a result.']);
+    end
+end
+
+% Adjust timestep (downward) so that t(end) == T. M is number of time values
+M = fix(T/ k) + 2;
+k = T / (M - 1);
+
+% t values in grid
+t = 0:k:T;
+
+% Determine g (see paper by Holden, Raynaud), and pre-calculate fft(g).
+% Note that by replacing N by K = 1 / h the method works on arbitrary intervals.
+K = 1 / h;
+kappa = log( (1 + 2 * K^2 + sqrt(1 + 4*K^2)) / (2 * K ^ 2));
+c = 1 / (1 + 2 * K^2 * (1 - exp(-kappa)));
+I = 0:N - 1;
+g = c * (exp(-kappa * I) + exp(kappa * (I - N))) / (1 - exp(-kappa * N));
+fftg = transpose(fft(g));
+
+% Time solution evaluation run time
+ticstart = tic;
+progress = 0;
+if showprogress
+    w = waitbar(progress, 'Solving Camassa-Holm...');
+end
+
+% Difference operator matrices
+A = forwardbackward_matrix(N, h);
+Ainv = inv(A);
+B = backward_matrix(N, h);
+F = forward_matrix(N, h);
+C = central_matrix(N, h);
+
+[t, mcalc] = ode45(@mt, t, A * transpose(initialdata));
+M = length(t);
+U = zeros(M, N);
+
+for i = 1:M
+   U(i, :) = ifft(fftg' .* fft(mcalc(i, :)));
+end
+
+%% Exit
+
+elapsed = toc(ticstart);
+if printtiming
+    fprintf('Spent %4.2f seconds on solving equation.\n', elapsed);
+end
+
+if showprogress
+    close(w);
+end
+
+    function [] = updateProgress(fraction)
+        if showprogress
+            if (fraction - progress >= 0.05 || progress >= 1.0)
+                progress = fraction;
+                waitbar(fraction, w);
+            end
+        end
+    end
+
+    function [ mt ] = mt(t, m)
+       u = ifft(fftg .* fft(m));
+       mt = - (B * (m .* max(u, 0)) + F * (m .* min(u, 0))) - m .* (C * u);
+       %mt = - B * (m .* u) - m .* (C * u);
+    end
+end
+
+%% Finite differences
+function [ B ] = backward_matrix(N, h)
+B = spdiags([- ones(N, 1), ones(N, 1) ], -1:0, N, N) / h;
+B(1,N) = -1 / h;
+end
+
+function [ F ] = forward_matrix(N, h)
+F = - transpose(backward_matrix(N, h));
+end
+
+function [ C ] = central_matrix(N, h)
+e = ones(N, 1);
+C = spdiags([-e/(2*h), e/(2*h)], [-1, 1], N, N);
+C(1,N) = -1/(2*h);
+C(N,1) = 1/(2*h);
+end
+
+function [ A ] = forwardbackward_matrix(N, h)
+e = ones(N, 1);
+A = spdiags([-e, (h^2 + 2) * e, -e], -1:1, N, N) / (h^2);
+A(N,1) = -1/h^2;
+A(1,N) = -1/h^2;
+end
+
+function [ k ] = calculatetimestep(initial, a, b, h)
+
+areaAbove = integral(@(x) max(initial(x), 0), a, b);
+areaBelow = - integral(@(x) min(initial(x), 0), a, b);
+A = max(areaAbove, areaBelow);
+c = A / 2;
+
+% Use the CFL condition and assume the maximum height is equal to the
+% velocity of the wave (assuming it is a wave).
+% Multiply by a heuristic "goodness factor" to overestimate its maximum velocity.
+k = h / (1.1 * c);
+
+end
+
+%% Parameter parsing for holdenraynaud
+function [ showprogress, printtiming, timestep ] = parse(options)
+% Parses additional options to holdenraynaud
+
+% Set default values for options
+showprogress = true;
+printtiming = true;
+timestep = 0;
+
+count = length(options);
+for k = 1:2:count
+    % Sanity checks
+    parameter = lower(options{k});
+    missingMessage = strcat('Missing parameter value for parameter ''', ...
+        parameter, '''.');
+    assert(k + 1 <= count, missingMessage);
+    
+    value = options{k + 1};
+    assert(~isempty(value), missingMessage);
+    
+    % Note: Lower-case for case insensitivity
+    switch parameter
+        case 'showprogress'
+            assertlogical(parameter, value);
+            showprogress = value;
+        case 'printtiming'
+            assertlogical(parameter, value);
+            printtiming = value;
+        case 'dt'
+            assertnumber(parameter, value);
+            timestep = value;
+    end
+    
+    
+end
+
+    function assertlogical(parameter, value)
+        assert(islogical(value), strcat('Parameter value for parameter ''', ...
+            parameter, ''' is not logical (true/false).'));
+    end
+
+    function assertnumber(parameter, value)
+        assert(isscalar(value) && isnumeric(value), strcat('Parameter value for parameter ''', ...
+            parameter, ''' is not a numerical scalar.'));
+    end
+end
---
+++ implementations/holden-raynaud/testsuite_semidiscrete.m
@@ -0,0 +1,77 @@
+% Make sure to clear any remnants of U as we'll need the memory
+clear U;
+
+%% Imports
+% Make utilities available
+path = fileparts(which(mfilename));
+addpath(fullfile(path, '../../util'));
+clear path;
+
+%% Configuration
+% Spatial resolution
+N = 1024;
+% Maximum time value
+T = 50;
+
+xmin = -10;
+xmax = 10;
+
+% Compression settings
+% nx = number of x values in compressed matrix
+nx = 500;
+% nt = number of t values in compressed matrix
+nt = 500;
+
+%% Initial condition (as a function of x)
+a = 1;
+peakon = @(x, t) exp(-abs(x - t));
+%initial = @(x) cosh(min(x, a - x)) / sinh(a / 2);
+
+%initial = @(x) [cosh(min(x(1:floor(end/2)), a - x(1:floor(end/2)))), ...
+%    repmat(cosh(min(x(floor(end / 2 + 1)), a - x(floor(end / 2 + 1)))), 1, ...
+%    length(x) - floor(length(x) / 2))] - ...
+%    cosh(min(x(floor(end / 2 + 1)), a - x(floor(end / 2 + 1))));
+%initial = @(x) cosh(min(x, a - x)) + circshift(0.5 * cosh(min(x, a - x)), ...
+%    repmat(round(length(x) / 2), length(x), 1));
+%initial = @(x) exp(-10 * abs(x - 1.2)) + exp(-10 * abs(x - 0.2));
+%initial = @(x) cosh(min(x, a - x)) / sinh(a / 2);
+%initial = @(x) 3 * exp(-abs(x)) - 3 * exp(-abs(x - 10));
+%initial = @(x) exp(-abs(x + 2)) + exp(-abs(x - 2));
+%initial = @(x) 5 * exp(-abs(x + 5)) + 5 * exp(-abs(x - 25));
+%initial = @(x) exp(-abs(x + 5)) - exp(-abs(x - 5));
+%initial = @(x) sin(2 * pi * x / 20) + 1;
+%initial = @(x) sin(pi * x / 2);
+initial = @(x) peakon(x, 0);
+%initial = @(x) sin(pi * x / 10);
+% m1 = (exp(-5) + 6) / (2 * exp(-5) + 3);
+% m2 = (exp(-5) + 2/3) / (exp(-5) + 3);
+% x1 = log(18 * exp(-10) / (exp(-5)) + 6);
+% x2 = log(40 * exp(-10) + 60 * exp(-5));
+% initial = @(x) m1 * exp(-abs(x - x1)) + ...
+%      m2 * exp(-abs(x - x2));
+
+%% Solve equation
+[U, x, tu] = holdenraynaud(N, T, [xmin, xmax], initial, 'showprogress', true);
+[V, x, tv] = holdenraynaud_semidiscrete(N, T, [xmin, xmax], initial, 'showprogress', true);
+% M = size(U, 1);
+
+% %% Create reference solution
+% R = zeros(M, N);
+% for j = 1:M
+%    R(j, :) = peakon(x, t(j));
+% end
+
+%% Compression
+% Don't expand matrix if result matrix is smaller
+% nx = min(nx, N);
+% nt = min(nt, M);
+[Z, xcomp, tcomp] = compress(x, tu, U, nx, nt);
+[Y, ~, ~] = compress(x, tv, V, nx, nt);
+
+%% Plotting
+figure
+surf(xcomp, tcomp, Z)
+shading flat;
+animatedplot(xcomp, tcomp, Z, Y)
+xlabel('x')
+ylabel('y')
