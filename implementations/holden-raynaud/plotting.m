410782f476a4701db8926d93be3faae10386d646
Jon Christian Halvorsen <jchalvorsen@gmail.com>
2014-04-01 09:08:07 +0200

    Higher resolution figures

--- implementations/holden-raynaud/plotting.m
+++ implementations/holden-raynaud/plotting.m
@@ -11,44 +11,54 @@ peakonAntipeakon = @(x,t) peakon(x,t,1,0) - antipeakon(x,t,1,20);
 
 xmin = -5;
 xmax = 10;
-x = linspace(xmin,xmax,500);
+x = linspace(xmin,xmax,2000);
 
 % Single peakon
 t = [ 0 5 10 ];
-figure
+p1 = figure;
 hold on
 plot(x,peakon(x,t(1),1,0),'b')
 plot(x,peakon(x,t(2),1,0),'b--')
-legend('t = 0', 't = 5', 'Location', 'NorthEast')
+%legend('t = 0', 't = 5', 'Location', 'NorthEast')
+
 
 % Double peakon
-figure
+p2 = figure;
 hold on
 plot(x,doublePeakon(x,t(1)),'b')
 plot(x,doublePeakon(x,t(2)),'b--')
-legend('t = 0', 't = 5', 'Location', 'NorthEast')
+%legend('t = 0', 't = 5', 'Location', 'NorthEast')
+
 
 % Double peakon interaction
 xmin = -5;
 xmax = 25;
-x = linspace(xmin,xmax,500);
-figure
+x = linspace(xmin,xmax,2000);
+p3 = figure;
 hold on
 plot(x,peakonOvertake(x,t(1)),'b')
 plot(x,peakonOvertake(x,t(2)),'b--')
 plot(x,peakonOvertake(x,t(3)),'b-.')
-legend('t = 0', 't = 5', 't = 10', 'Location', 'NorthEast')
+%legend('t = 0', 't = 5', 't = 10', 'Location', 'NorthEast')
+
 
 t = [0 10 15];
 % Peakon antipeakon interaction.
-figure
+p4 = figure;
 subplot(3,1,1)
 plot(x,peakonAntipeakon(x,t(1)),'b')
-legend('t = 0', 'Location', 'NorthEast')
+%legend('t = 0', 'Location', 'NorthEast')
 subplot(3,1,2)
 plot(x,peakonAntipeakon(x,t(2)),'b--')
 axis([xmin xmax -1 1])
-legend('t = 10', 'Location', 'NorthEast')
+%legend('t = 10', 'Location', 'NorthEast')
 subplot(3,1,3)
 plot(x,peakonAntipeakon(x,t(3)),'b-.')
-legend('t = 15', 'Location', 'NorthEast')
+%legend('t = 15', 'Location', 'NorthEast')
+
+
+% print images to file
+% print(p1,'-dpng','-r400','peakon')
+% print(p2,'-dpng','-r400','doublepeakon')
+% print(p3,'-dpng','-r400','peakonovertake')
+ print(p4,'-dpng','-r400','peakonantipeakon')
